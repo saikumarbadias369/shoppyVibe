1d02e2b2dfa893a9d3824c303f495b05cf2a499f
sai kumar <saikumarbadisa122@gmail.com>
2024-04-13 18:48:49 +0530

    updated variables

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const app=require("..")
-const connectDb =require('./config/db')
+const app=require("./src")
+const connectDb =require('./src/config/db')
 require('dotenv').config()
 
 app.listen(process.env.PORT,async()=>{
     await connectDb();
     console.log("sai's server running on PORT: ",process.env.PORT)
 })
+
--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 const express=require("express");
 const cors=require("cors");
 
-const authRoutes=require('./src/routes/auth.route.js')
+console.log('before connecting1')
+const authRoutes=require('./routes/auth.route.js')
 
 const app= express()
-
+console.log('before connecting3')
 app.use(express.json())
 app.use(cors())
-
+console.log('before connecting2')
 app.get("/",(req,res)=>{
     return res.status(200).send({message:"welcome to ecommerce api",status:true})   
 })
 
-
+console.log('before connecting4')
 app.use('/auth',authRoutes)
 
 //loss400/auth/login
-const userRouter=require('./src/routes/user.route.js')
+const userRouter=require('./routes/user.route.js')
 app.use('/api/users',userRouter)
-
-const cartRouter=require('./src/routes/cart.route.js')
+console.log('before connecting5')
+const cartRouter=require('./routes/cart.route.js')
 app.use('/api/cart',cartRouter)
 
-const cartItemRouter=require('./src/routes/cartItem.route.js')
+const cartItemRouter=require('./routes/cartItem.route.js')
 app.use('/api/cart_items',cartItemRouter)
 
-const   productRouter=require('./src/routes/product.route.js')
+const   productRouter=require('./routes/product.route.js')
 app.use('/api/products',productRouter)
 
 module.exports=app;
